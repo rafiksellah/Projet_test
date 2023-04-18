a9f4bd54ac518ceab94c80184378c436265829c3
rafiksellah <rafik@theagency-dz.com>
2023-04-18 15:08:26 +0100

    partie frontend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\OffreDemploiRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(OffreDemploiRepository $offreDemploiRepository): Response
     {
+        $offre_emplois = $offreDemploiRepository->findBy(['statut'=>1]);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'offre_emplois' => $offre_emplois,
         ]);
     }
 }
